7f482096cb3092e4a359fe09ed0c68e3af60c5ce
SahiraMR <samedina91@ucatolica.edu.co>
2025-05-07 05:09:36 -0500

    primera crud

--- crud/usuario_crud.py
+++ crud/usuario_crud.py
@@ -1,1 +1,19 @@
+import csv
+from typing import List, Optional
+from models.usuario import Usuario
+
+RUTA_CSV = "data/usuarios.csv"
+
+def leer_usuarios() -> List[Usuario]:
+    usuarios = []
+    with open(RUTA_CSV, newline='', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            usuarios.append(Usuario(**{
+                "id": int(row["id"]),
+                "nombre": row["nombre"],
+                "correo": row["correo"],
+                "tipo": row["tipo"]
+            }))
+    return usuarios
 
