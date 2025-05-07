34ec5c54a345d1a0188daf33aa50601d1d2e7c67
SahiraMR <samedina91@ucatolica.edu.co>
2025-05-07 05:06:40 -0500

    espacio para usuario

--- models/usuario.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Literal
-
-
-class Usuario(BaseModel):
-    id: int
-    nombre: str
-    correo: str
-    tipo: Literal["Premium", "Normal", "Inactivo"]
